33fac02a6247e2b42b73e4914afdaa5a231ba937
adelchan07 <66882888+adelchan07@users.noreply.github.com>
2020-09-02 23:18:33 -0700

    Update reward.py

--- executive/reward.py
+++ executive/reward.py
@@ -32,3 +32,37 @@ class getPrimitiveOptionReward(object):
 			reward += self.goalReward
 
 		return reward
+	
+class getLandmarkOptionReward(object):
+	def __init__(self, actionCost,moveCost, goalStates, goalReward, getNextState):
+		self.stateSet = stateSet
+
+		self.actionCost = actionCost
+		self.moveCost = moveCost
+		self.goalStates = goalStates
+		self.goalReward = goalReward
+
+		self.getNextState = getNextState
+
+	def __call__(self, state, optionPolicy, terminationCondition):
+
+		reward = self.moveCost + self.getCumulativeCost(state, optionPolicy, terminationCondition)
+
+		if terminationCondition in self.goalStates:
+			reward += self.goalReward
+
+		return reward
+
+	def getCumulativeCost(self, state, optionPolicy, terminationCondition):
+
+		stepsTaken = 0
+		currentState = state
+
+		while currentState != terminationCondition:
+			stepsTaken += 1
+			action = optionPolicy[currentState]
+			nextState = self.getNextState(currentState, action, self.stateSet)
+			currentState = nextState
+
+		cumulativeCost = stepsTaken*self.actionCost
+		return cumulativeCost
