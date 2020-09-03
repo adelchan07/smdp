fe2cd086870cc72f4b48e3c350f27fa3d32f0de6
adelchan07 <66882888+adelchan07@users.noreply.github.com>
2020-09-02 23:10:35 -0700

    Add files via upload

---
+++ transition.py
@@ -0,0 +1,23 @@
+"""
+Created on Mon Aug 17 13:54:03 2020
+
+@author: adelphachan
+
+transition.py
+
+general function:
+ - used once for each primitive option
+ - used for x amount of steps required to get to termination condition for each landmark option 
+"""
+
+def getNextState(state, action, stateSet):
+
+	xCoord = state[0] + action[0]
+	yCoord = state[1] + action[1]
+
+	result = (xCoord, yCoord)
+
+	if result in stateSet:
+		return result
+	else:
+		return state
