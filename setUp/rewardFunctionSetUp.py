2c87a148a3037d73e72919150b00bab58d36d689
adelchan07 <66882888+adelchan07@users.noreply.github.com>
2020-09-08 12:29:33 -0700

    Update rewardFunctionSetUp.py

--- setUp/rewardFunctionSetUp.py
+++ setUp/rewardFunctionSetUp.py
@@ -5,3 +5,24 @@ rewardFunctionSetUp.py
 
 set up dictionary of corresponding reward function for each type of option
 """
+
+class setUpRewardFunction(object):
+  
+  def __init__(self, optionType, getPrimitiveOptionReward, getLandmarkOptionReward):
+    self.optionType = optionType
+    
+    self.primitiveReward = getPrimitiveOptionReward
+    self.landmarkReward = getLandmarkOptionReward
+   
+  def __call__(self):
+    rewards = {option: self.getRewardFunction(option) for option in self.optionType.keys()}
+    return rewards
+  
+  def getRewardFunction(self, option):
+    type = self.optionType[option]
+    
+    if type == "primitive":
+      return self.primitiveReward
+    
+    if type == "landmark":
+      return self.landmarkReward
