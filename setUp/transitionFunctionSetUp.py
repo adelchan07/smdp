3d3d0a6b169014ee6c94f5d169a8ff35adef9667
adelchan07 <66882888+adelchan07@users.noreply.github.com>
2020-09-08 12:26:39 -0700

    Update transitionFunctionSetUp.py

--- setUp/transitionFunctionSetUp.py
+++ setUp/transitionFunctionSetUp.py
@@ -5,3 +5,25 @@ transitionFunctionSetUp.py
 
 set up dictionary of corresponding transition function for each type of option
 """
+
+class setUpTransitionFunction(object):
+  
+  def __init__(self, optionType, getPrimitiveSPrime, getLandmarkSPrime):
+    self.optionType = optionType
+    
+    self.primitiveTransition = getPrimitiveSPrime
+    self.landmarkTransition = getLandmarkSPrime
+   
+  def __call__(self):
+    transitions = {option: self.getTransitionFunction(option) for option in self.optionType.keys()}
+    return transitions
+  
+  def getTransitionFunction(self, option):
+    type = self.optionType[option]
+    
+    if type == "primitive":
+      return self.getPrimitiveSPrime
+    
+    if type == "landmark":
+      return self.getLandmarkSPrime
+ 
