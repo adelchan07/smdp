9224fb9a71d0d22e7166e49fcb2a14db92b29286
adelchan07 <66882888+adelchan07@users.noreply.github.com>
2020-09-10 14:30:31 -0700

    Update testLandmarkOptionSetUp.py

--- tests/testLandmarkOptionSetUp.py
+++ tests/testLandmarkOptionSetUp.py
@@ -82,12 +82,12 @@ class TestSetUpLandmark(unittest.TestCase):
 		for key in calculatedDictionary.keys():
 		    self.assertAlmostEqual(calculatedDictionary[key], expectedDictionary[key], places=places)
 	
-	@data(((1,1), {(0,1): 0.5, (0,-1): 0,5}))
+	@data(((1,1), {(0,1): 0.5, (0,-1): 0.5}))
 	@unpack
 	def test_AtLandmark(self, state, expectedResult):
 		self.assertNumericDictAlmostEqual(self.policies[state], expectedResult)
 		
-	@data(((3,0), {(-1,0): 0.5, (0,1): 0,5}), ((3,2), {(-1,0): 0.5, (0,-1): 0.5}))
+	@data(((3,0), {(-1,0): 0.5, (0,1): 0.5}), ((3,2), {(-1,0): 0.5, (0,-1): 0.5}))
 	@unpack
 	def test_Boundary(self, state, expectedResult):
 		self.assertNumericDictAlmostEqual(self.policies[state], expectedResult)
