c1cfdb260e077ba19030e2687e6b4c772cf234c7
adelchan07 <66882888+adelchan07@users.noreply.github.com>
2020-09-10 13:13:41 -0700

    Update landmarkOptionSetUp.py

--- setUp/landmarkOptionSetUp.py
+++ setUp/landmarkOptionSetUp.py
@@ -1,81 +1,67 @@
-"""
-Created on Sun Aug 9 12:04:03 2020
-
-@author: adelphachan
-
-landmarkOptionSetUp.py
-
-return = dictionary of landmark options
---> append values onto a dictionary that already exists
-	(allows us to be able to add any type of option necessary to the same general option dictionary)
-
-constants for all dictionaries:
-- transition table / stateSet (already updated with blockList)
-- actionCost 
-- goalReward
-- getLandmarkReward callable
-- getLandmarkPolicy callable
-
-landmarks = {"name": location}
-
-"""
 import sys
-import os
-dirName = os.path.dirname(__file__)
-sys.path.append(os.path.join(dirName, 'basics', ""))
+sys.path.append('..') 
 
 import numpy as np
-import transitionTable as tt
-import rewardTable as rt
-import valueIteration as vi
+import unittest
+from ddt import ddt, data, unpack
+import valueIteration as targetCode 
 
-#general helper functions 
-def merge(dictionary1, dictionary2):
-	
-	result = {**dictionary1, **dictionary2}
-	return result
+@ddt
+class TestSimpleValueIteration(unittest.TestCase):
+  def setUp(self):
+    primitiveOptions = {'up': (0,1), 'down': (0,-1), 'right': (1,0), 'left': (-1,0)}
+    stateSet = [(i,j) for i in range(2) for j in range(2)]
+    getNextState = targetCode.tf.getNextState
+    primitiveSPrime = targetCode.tf.GetPrimitiveSPrime(primitiveOptions, stateSet, getNextState)
 
-#valueIteration helper classes
-class GetLandmarkPolicy(object):
-	def __init__(self, gamma, convergenceTolerance):
-		self.gamma = gamma
-		self.convergenceTolerance = convergenceTolerance
+    optionTerminations = {"LL": (0,0), "UL": (0,1), "LR": (1,0), "UR": (1,1)}
+    landmarkSPrime = targetCode.tf.GetLandmarkSPrime(optionTerminations)
+
+    optionSPrime = {'up': primitiveSPrime, 'down':primitiveSPrime, 'left': primitiveSPrime, 'right': primitiveSPrime,'LL': landmarkSPrime, 'UL': landmarkSPrime, 'LR': landmarkSPrime, 'UR': landmarkSPrime} 
+
+    transition = targetCode.tf.TransitionFunction(optionSPrime)
+
+    actionCost = -1
+    moveCost = -3
+    goalStates = [(0,0)]
+    goalReward = 10
+    landmarkPolicies = {'LL': {(0, 0): (0, -1), (0, 1): (0, -1), (1, 0): (-1, 0), (1, 1): (0, -1)}, 'UL': {(0, 0): (0, 1), (0, 1): (0, 1), (1, 0): (0, 1), (1, 1): (-1, 0)}, 'LR': {(0, 0): (1, 0), (0, 1): (1, 0), (1, 0): (0, -1), (1, 1): (0, -1)}, 'UR': {(0, 0): (0, 1), (0, 1): (1, 0), (1, 0): (0, 1), (1, 1): (0, 1)}}
+    primitiveReward = targetCode.rf.GetPrimitiveOptionReward(actionCost, moveCost, goalStates, goalReward, primitiveSPrime)
+    landmarkReward = targetCode.rf.GetLandmarkOptionReward(actionCost, moveCost, goalStates, goalReward, landmarkPolicies, landmarkSPrime, getNextState)
+
+    optionReward = {'up': primitiveReward, 'down': primitiveReward, 'left': primitiveReward, 'right': primitiveReward, 'LL':landmarkReward, 'UL': landmarkReward, 'UR': landmarkReward, 'LR': landmarkReward} 
+    reward = targetCode.rf.RewardFunction(optionReward)
+
+    optionSpace = {(0, 0): ['LL', 'UL', 'LR', 'UR', 'up', 'down', 'left', 'right'], (0, 1): ['LL', 'UL', 'LR', 'UR', 'up', 'down', 'left', 'right'], (1, 0): ['LL', 'UL', 'LR', 'UR', 'up', 'down', 'left', 'right'], (1, 1): ['LL', 'UL', 'LR', 'UR', 'up', 'down', 'left', 'right']}
+    optionSpace = targetCode.osf.OptionSpaceFunction(optionSpace)
+
+    gamma = 0.9
+    convergence = 0.0001
+
+    bellmanUpdate = targetCode.BellmanUpdate(stateSet, optionSpace, transition, reward, gamma)
+    valueItSetUp = targetCode.ValueIteration(stateSet, optionSpace, convergence, bellmanUpdate)
+    V = valueItSetUp()
+    policySetUp = targetCode.GetPolicy(stateSet, optionSpace, transition, reward, gamma, V, convergence)
+    self.policy = {s: policySetUp(s) for s in stateSet}
+
+  def assertNumericDictAlmostEqual(self, calculatedDictionary, expectedDictionary, places=7):
+    self.assertEqual(calculatedDictionary.keys(), expectedDictionary.keys())
+    for key in calculatedDictionary.keys():
+        self.assertAlmostEqual(calculatedDictionary[key], expectedDictionary[key], places=places)
 	
-	def __call__(self, transitionTable, rewardTable):
-		transitionFunction = tt.TransitionFunction(transitionTable)
-		rewardFunction = rt.RewardFunction(rewardTable)
-		stateSpace = list(transitionTable.keys())
-		actionSpaceFunction = lambda s: list(transitionTable.get(s).keys())
-		
-		bellmanUpdate = vi.BellmanUpdate(stateSpace, actionSpaceFunction, transitionFunction, rewardFunction, self.gamma)
-		valueItSetUp = vi.ValueIteration(stateSpace, actionSpaceFunction, self.convergenceTolerance, bellmanUpdate)
-		V = valueItSetUp()
-		policySetUp = vi.GetPolicy(stateSpace, actionSpaceFunction, transitionFunction, rewardFunction, self.gamma, V, self.convergenceTolerance)
-		
-		return {s: policySetUp(s) for state in stateSpace}
-		
-#main landmark option set up class
-class SetUpLandmark(object): 
-	def __init__(self, landmarkLocation, landmarkStateSet, actionSet, getTransitionTable, getRewardTable, getLandmarkPolicy, merge):
-		self.landmarkLocation = landmarkLocation
-		self.landmarkStateSet = landmarkStateSet
-		
-		self.actionSet = actionSet
-		
-		self.getTransitionTable = getTransitionTable
-		self.getRewardTable = getRewardTable
-		self.getLandmarkPolicy = getLandmarkPolicy
-		self.merge = merge
-	def __call__(self, existingOptions):
-		landmark = {option: self.getOptionPolicy(option) for option in self.landmarkLocation.keys()}
-		
-		return self.merge(landmark, existingOptions)
-	
-	def getOptionPolicy(self, option):
-		stateSet = self.landmarkStateSet[option]
-		transitionTable = self.getTransitionTable(stateSet)
-		
-		goalStates = list(self.landmarkLocation[option])
-		rewardTable = self.getRewardTable(transitionTable, goalStates)
-		
-		return self.getLandmarkPolicy(transitionTable, rewardTable)
+  @data(((0,0), {'LL': 1.0}))
+  @unpack
+  def test_AtGoal(self, state, expectedResult):
+    self.assertNumericDictAlmostEqual(self.policy[state], expectedResult)
+  
+  @data(((1,1), {'LL': 1.0}), ((0,1), {'LL': 0.5, 'down': 0.5}), ((1, 0), {'LL': 0.5, 'left': 0.5}))
+  @unpack
+  def test_OutsideGoal(self, state, expectedResult):
+    self.assertNumericDictAlmostEqual(self.policy[state], expectedResult)
+
+  def tearDown(self):
+    pass
+  
+if __name__ == '__main__':
+	unittest.main(verbosity=2)
+
