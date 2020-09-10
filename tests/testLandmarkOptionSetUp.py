2a6a8d36f99ab437c07798b99b03a7b06ed997fe
adelchan07 <66882888+adelchan07@users.noreply.github.com>
2020-09-10 14:28:09 -0700

    Update testLandmarkOptionSetUp.py

--- tests/testLandmarkOptionSetUp.py
+++ tests/testLandmarkOptionSetUp.py
@@ -53,62 +53,50 @@ class TestGetLandmarkPolicy(unittest.TestCase):
 
 	def tearDown(self):
 		pass
-
+	
 @ddt
 class TestSetUpLandmark(unittest.TestCase):
 	def setUp(self):
-		landmarkLocation = {"h1": (3,3), "h2": (5,2), "h3": (5,6), "h4": (9,4)}
-		landmarkStateSet = {'h1': [(1, 1), (1, 2), (2, 1), (2, 2), (3, 1), (3, 2), (4, 1), (4, 2), (1, 4), (1, 5), (1, 6), (1, 7), (2, 4), (2, 5), (2, 6), (2, 7), (3, 4), (3, 5), (3, 6), (3, 7), (4, 4), (4, 5), (4, 6), (4, 7), (3, 3)], 'h2': [(1, 1), (1, 2), (2, 1), (2, 2), (3, 1), (3, 2), (4, 1), (4, 2), (6, 1), (6, 2), (6, 3), (7, 1), (7, 2), (7, 3), (8, 1), (8, 2), (8, 3), (9, 1), (9, 2), (9, 3), (10, 1), (10, 2), (10, 3), (11, 1), (11, 2), (11, 3), (5, 2)], 'h3': [(1, 4), (1, 5), (1, 6), (1, 7), (2, 4), (2, 5), (2, 6), (2, 7), (3, 4), (3, 5), (3, 6), (3, 7), (4, 4), (4, 5), (4, 6), (4, 7), (6, 5), (6, 6), (6, 7), (7, 5), (7, 6), (7, 7), (8, 5), (8, 6), (8, 7), (9, 5), (9, 6), (9, 7), (10, 5), (10, 6), (10, 7), (11, 5), (11, 6), (11, 7), (5, 6)], 'h4': [(6, 5), (6, 6), (6, 7), (7, 5), (7, 6), (7, 7), (8, 5), (8, 6), (8, 7), (9, 5), (9, 6), (9, 7), (10, 5), (10, 6), (10, 7), (11, 5), (11, 6), (11, 7), (6, 1), (6, 2), (6, 3), (7, 1), (7, 2), (7, 3), (8, 1), (8, 2), (8, 3), (9, 1), (9, 2), (9, 3), (10, 1), (10, 2), (10, 3), (11, 1), (11, 2), (11, 3), (9, 4)]}
-		
+		landmarkLocation = {"hall": (1,1)}
+		landmarkStateSet = [(0, 0), (0, 1), (0, 2), (1, 1), (2, 0), (2, 1), (2, 2), (3, 0), (3, 1), (3, 2)]
+
 		actionSet = [(0,1), (0,-1), (1,0), (-1,0)]
 		getTransitionTable = targetCode.tt.CreateTransitionTable(actionSet)
-		
+
 		actionCost = -1
 		goalReward = 10
 		getRewardTable = targetCode.rt.CreateRewardTable(actionSet, actionCost, goalReward)
-		
+
 		gamma = 0.9
-		convergenceTolerance = .000001
+		convergenceTolerance = 0.00001
 		getLandmarkPolicy = targetCode.GetLandmarkPolicy(gamma, convergenceTolerance)
-		
+
 		merge = targetCode.merge
-		
+
 		existingOptions = {}
 		setUp = targetCode.SetUpLandmark(landmarkLocation, landmarkStateSet, actionSet, getTransitionTable, getRewardTable, getLandmarkPolicy, merge)
-		self.landmarkPolicies = setUp(existingOptions)
+		self.policies = setUp(existingOptions)
 	
 	def assertNumericDictAlmostEqual(self, calculatedDictionary, expectedDictionary, places=7):
 		self.assertEqual(calculatedDictionary.keys(), expectedDictionary.keys())
 		for key in calculatedDictionary.keys():
 		    self.assertAlmostEqual(calculatedDictionary[key], expectedDictionary[key], places=places)
 	
-	#testing landmark policies --> make sure locations surrounding location of interest AND at location of interestare correct
-	@data(((3,2), (0,1)), ((3,4), (0,-1)), ((3,3), (1,0)))
+	@data(((1,1), {(0,1): 0.5, (0,-1): 0,5}))
 	@unpack
-	def test_LandmarkHallway1(self, state, expectedAction):
-		self.assertEqual(self.landmarkPolicies["h1"][state], expectedAction)
-
-	@data(((4,2), (1,0)), ((5,2), (0,1)), ((6,2), (-1,0)))
+	def test_AtLandmark(self, state, expectedResult):
+		self.assertNumericDictAlmostEqual(self.policies[state], expectedResult)
+		
+	@data(((3,0), {(-1,0): 0.5, (0,1): 0,5}), ((3,2), {(-1,0): 0.5, (0,-1): 0,5}))
 	@unpack
-	def test_LandmarkHallway2(self, state, expectedAction):
-		self.assertEqual(self.landmarkPolicies["h2"][state], expectedAction)
-
-	@data(((4,6), (1,0)), ((5,6), (0,1)), ((6,6), (-1,0)))
+	def test_Boundary(self, state, expectedResult):
+		self.assertNumericDictAlmostEqual(self.policies[state], expectedResult)
+		
+	@data(((0,1), {(1,0): 1.0}), ((2,1), {(-1,0): 1.0}))
 	@unpack
-	def test_LandmarkHallway3(self, state, expectedAction):
-		self.assertEqual(self.landmarkPolicies["h3"][state], expectedAction)
-
-	@data(((9,3), (0,1)), ((9,4), (1,0)), ((9,5), (0,-1)))
-	@unpack
-	def test_LandmarkHallway4(self, state, expectedAction):
-		self.assertEqual(self.landmarkPolicies["h4"][state], expectedAction)
-
-	#testing boundary locations don't go off the specified grid
-	@data(("h1", (3,1), (0,1)), ("h2", (6,1), (0,1)), ("h3", (4,7), (0,-1)), ("h4", (10,7), (0,-1)))
-	@unpack
-	def testLandmarkBoundaries(self, policy, state, expectedAction):
-		self.assertEqual(self.landmarkPolicies[policy][state], expectedAction)
-
+	def test_Inner(self, state, expectedResult):
+		self.assertNumericDictAlmostEqual(self.policies[state], expectedResult)
+		
 	def tearDown(self):
 		pass
  
