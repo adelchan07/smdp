d4f427d575994d0871eb1083922282c8f03dacdd
adelchan07 <66882888+adelchan07@users.noreply.github.com>
2020-09-10 12:59:58 -0700

    Update testTransition.py

--- tests/testTransition.py
+++ tests/testTransition.py
@@ -42,17 +42,22 @@ class TestPrimitiveSPrime(unittest.TestCase):
 		stateSet = [(i,j) for i in range(2) for j in range(2)]
 		getNextState = targetCode.getNextState
 
-		self.getSPrime = targetCode.getPrimitiveSPrime(primitivePolicies, stateSet, getNextState)
+		self.getSPrime = targetCode.GetPrimitiveSPrime(primitivePolicies, stateSet, getNextState)
 	
 	@data(((0,0), 'left', (0,0)), ((1,1), 'up', (1,1)), ((1,0), 'down', (1,0)))
 	@unpack
-	def test_Boundary(self, state, option, expectedSPrime):
-		self.assertEqual(self.getSPrime(state, option), expectedSPrime)
+	def test_Boundary(self, state, option, sPrime):
+		self.assertEqual(self.getSPrime(state, option, sPrime), 1)
 	
 	@data(((0,0), 'right', (1,0)), ((1,1), 'left', (0,1)), ((0,1), 'down', (0,0)))
 	@unpack
-	def test_Inner(self, state, option, expectedSPrime):
-		self.assertEqual(self.getSPrime(state, option), expectedSPrime)
+	def test_Inner(self, state, option, sPrime):
+		self.assertEqual(self.getSPrime(state, option, sPrime), 1)
+	
+	@data(((0,0), 'left', (1,0)), ((1,1), 'up', (0,1)), ((1,0), 'down', (1,1)))
+	@unpack
+	def test_WrongSPrime(self, state, option, sPrime):
+		self.assertEqual(self.getSPrime(state, option, sPrime), 0)
 	
 	def tearDown(self):
 		pass
@@ -61,17 +66,22 @@ class TestPrimitiveSPrime(unittest.TestCase):
 class TestLandmarkSPrime(unittest.TestCase):
 	def setUp(self):
 		optionTerminations = {"LL": (0,0), "UL": (0,1), "LR": (1,0), "UR": (1,1)}
-		self.getSPrime = targetCode.getLandmarkSPrime(optionTerminations)
+		self.getSPrime = targetCode.GetLandmarkSPrime(optionTerminations)
 	
 	@data(((0,0), "LL", (0,0)), ((0,1), "UL", (0,1)), ((1,0), "LR", (1,0)), ((1,1), "UR", (1,1)))
 	@unpack
-	def test_AtLandmark(self, state, option, expectedSPrime):
-		self.assertEqual(self.getSPrime(state, option), expectedSPrime)
+	def test_AtLandmark(self, state, option, sPrime):
+		self.assertEqual(self.getSPrime(state, option, sPrime), 1)
 		
 	@data(((1,1), "LL", (0,0)), ((0,0), "UL", (0,1)), ((0,0), "LR", (1,0)), ((1,0), "UR", (1,1)))
 	@unpack
-	def test_NeedToMove(self, state, option, expectedSPrime):
-		self.assertEqual(self.getSPrime(state, option), expectedSPrime)
+	def test_NeedToMove(self, state, option, sPrime):
+		self.assertEqual(self.getSPrime(state, option, sPrime), 1)
+	
+	@data(((0,0), "LL", (1,0)), ((0,1), "UL", (1,1)), ((1,0), "LR", (1,1)), ((1,1), "UR", (1,0)))
+	@unpack
+	def test_WrongSPrime(self, state, option, sPrime):
+		self.assertEqual(self.getSPrime(state, option, sPrime), 0)
 	
 	def tearDown(self):
 		pass
@@ -83,14 +93,14 @@ class TestTransitionFunction(unittest.TestCase):
 		primitivePolicies = {'up': (0,1), 'down': (0,-1), 'right': (1,0), 'left': (-1,0)}
 		stateSet = [(i,j) for i in range(2) for j in range(2)]
 		getNextState = targetCode.getNextState
-		primitiveSPrime = targetCode.getPrimitiveSPrime(primitivePolicies, stateSet, getNextState)
+		primitiveSPrime = targetCode.GetPrimitiveSPrime(primitivePolicies, stateSet, getNextState)
 		
 		optionTerminations = {"LL": (0,0), "UL": (0,1), "LR": (1,0), "UR": (1,1)}
-		landmarkSPrime = targetCode.getLandmarkSPrime(optionTerminations)
+		landmarkSPrime = targetCode.GetLandmarkSPrime(optionTerminations)
 		
 		optionSPrime = {'up': primitiveSPrime, 'down':primitiveSPrime, 'left': primitiveSPrime, 'right': primitiveSPrime,'LL': landmarkSPrime, 'UL': landmarkSPrime, 'LR': landmarkSPrime, 'UR': landmarkSPrime} 
 		
-		self.transitionFunction = targetCode.transitionFunction(optionSPrime)
+		self.transitionFunction = targetCode.TransitionFunction(optionSPrime)
 	
 	@data(((1,1), "LL", (1,0)), ((1,1), "up", (0,0)), ((0,0), "right", (1,1)))
 	@unpack
