9a3588b5dd3d2e22bac510e6f20a9350b32f9eb6
adelchan07 <66882888+adelchan07@users.noreply.github.com>
2020-09-10 17:42:08 -0700

    Update optionSpaceSetUp.py

--- setUp/optionSpaceSetUp.py
+++ setUp/optionSpaceSetUp.py
@@ -6,14 +6,19 @@ Created on Wed Sep 3 13:25:45 2020
 optionSpaceSetUp.py
 
 set up dictionary of corresponding options available at each state
+
+primitiveOptions = list of primitive options (as they apply to the entire state space)
+	ie. ['up', 'down', ...]
+landmarkPolicies = {landmark: (policy)}
 """
 
 import numpy as np 
 
 class OptionSpaceSetUp(object):
-	def __init__(self, stateSet, optionsDictionary):
+	def __init__(self, stateSet, landmarkPolicies, primitiveOptions):
 		self.stateSet = stateSet
-		self.optionsDictionary = optionsDictionary
+		self.primitiveOptions = primitiveOptions
+		self.landmarkPolicies = landmarkPolicies
 
 	def __call__(self):
 
@@ -22,12 +27,11 @@ class OptionSpaceSetUp(object):
 
 	def getAvailableOptions(self, state):
 
-		validOptions = []
+		validOptions = self.primitiveOptions
 
 		for option in self.optionsDictionary.keys():
 
-			#relevant states = option's policy.keys()
-			relevantStates = list(self.optionsDictionary[option].keys()) 
+			relevantStates = list(self.landmarkPolicies[option].keys()) 
 
 			if state in relevantStates:
 				validOptions.append(option)
