1bf67e4d8bcc694f4a4ea13740d707cc9a5089cd
adelchan07 <66882888+adelchan07@users.noreply.github.com>
2020-09-10 20:17:38 -0700

    Update testLandmarkOptionSetUp.py

--- tests/testLandmarkOptionSetUp.py
+++ tests/testLandmarkOptionSetUp.py
@@ -24,79 +24,80 @@ import landmarkOptionSetUp as targetCode
 class TestGetLandmarkPolicy(unittest.TestCase):
 	def setUp(self):
 		gamma = 0.9
-		convergenceTolerance = .000001
+		convergenceTolerance = 0.001
 		
-		transitionTable = {(0, 0): {(0, 1): {(0, 1): 1}, (0, -1): {(0, 0): 1}, (1, 0): {(1, 0): 1}, (-1, 0): {(0, 0): 1}}, (0, 1): {(0, 1): {(0, 1): 1}, (0, -1): {(0, 0): 1}, (1, 0): {(1, 1): 1}, (-1, 0): {(0, 1): 1}}, (1, 0): {(0, 1): {(1, 1): 1}, (0, -1): {(1, 0): 1}, (1, 0): {(1, 0): 1}, (-1, 0): {(0, 0): 1}}, (1, 1): {(0, 1): {(1, 1): 1}, (0, -1): {(1, 0): 1}, (1, 0): {(1, 1): 1}, (-1, 0): {(0, 1): 1}}}
+		stateSet = [(i,j) for i in range(2) for j in range(2)]
+		actionSet = [(0,1), (0,-1), (1,0), (-1,0)]
+		actionCost = -1
+		goalReward = 10
+		goalStates = [(0,0)]
 		
-		#LL = (0,0), UR = (1,1)
-		rewardTableLL =  {(0, 0): {(0, 1): {(0, 1): -1}, (0, -1): {(0, 0): 10}, (1, 0): {(1, 0): -1}, (-1, 0): {(0, 0): 10}}, (0, 1): {(0, 1): {(0, 1): -1}, (0, -1): {(0, 0): 10}, (1, 0): {(1, 1): -1}, (-1, 0): {(0, 1): -1}}, (1, 0): {(0, 1): {(1, 1): -1}, (0, -1): {(1, 0): -1}, (1, 0): {(1, 0): -1}, (-1, 0): {(0, 0): 10}}, (1, 1): {(0, 1): {(1, 1): -1}, (0, -1): {(1, 0): -1}, (1, 0): {(1, 1): -1}, (-1, 0): {(0, 1): -1}}}
-		rewardTableUR = {(0, 0): {(0, 1): {(0, 1): -1}, (0, -1): {(0, 0): -1}, (1, 0): {(1, 0): -1}, (-1, 0): {(0, 0): -1}}, (0, 1): {(0, 1): {(0, 1): -1}, (0, -1): {(0, 0): -1}, (1, 0): {(1, 1): 10}, (-1, 0): {(0, 1): -1}}, (1, 0): {(0, 1): {(1, 1): 10}, (0, -1): {(1, 0): -1}, (1, 0): {(1, 0): -1}, (-1, 0): {(0, 0): -1}}, (1, 1): {(0, 1): {(1, 1): 10}, (0, -1): {(1, 0): -1}, (1, 0): {(1, 1): 10}, (-1, 0): {(0, 1): -1}}}
+		setUp = targetCode.tt.CreateTransitionTable(actionSet)
+		transitionTable = setUp(stateSet)
+		
+		setUp = targetCode.rt.CreateRewardTable(actionSet, actionCost, goalReward)
+		rewardTable = setUp(transitionTable, goalStates)
 		
 		setUp = targetCode.GetLandmarkPolicy(gamma, convergenceTolerance)
-		self.policyLL = setUp(transitionTable, rewardTableLL)
-		self.policyUR = setUp(transitionTable, rewardTableUR)
+		self.policy = setUp(transitionTable, rewardTable)
 	
 	def assertNumericDictAlmostEqual(self, calculatedDictionary, expectedDictionary, places=7):
-		self.assertEqual(calculatedDictionary.keys(), expectedDictionary.keys())
-		for key in calculatedDictionary.keys():
-		    self.assertAlmostEqual(calculatedDictionary[key], expectedDictionary[key], places=places)
+    		self.assertEqual(calculatedDictionary.keys(), expectedDictionary.keys())
+    		for key in calculatedDictionary.keys():
+       	 		self.assertAlmostEqual(calculatedDictionary[key], expectedDictionary[key], places=places)
 	
-	@data(((0,0), {(0, -1): 0.5, (-1, 0): 0.5}), ((1,1), {(0, -1): 0.5, (-1, 0): 0.5}), ((1,0), {(0, -1): 1.0}))
+	@data(((0, 0), {(0, -1): 0.5, (-1, 0): 0.5}))
 	@unpack
-	def test_LandmarkRewardLL(self, state, expectedResult):
-		self.assertNumericDictAlmostEqual(self.policyLL[state], expectedResult)
-
-	@data(((0,0), {(0, 1): 0.5, (1, 0): 0.5}), ((1,1), {(0, 1): 0.5, (1, 0): 0.5}), ((1,0), {(1, 1): 1.0}))
+	def test_AtGoal(self, state, expectedResult):
+		self.assertNumericDictAlmostEqual(self.policy[state], expectedResult}
+	
+	@data(((0, 1), {(0, -1): 1.0}),((1, 1), {(0, -1): 0.5, (-1, 0): 0.5}))
 	@unpack
-	def test_LandmarkRewardUR(self, state, expectedResult):
-		self.assertNumericDictAlmostEqual(self.policyUR[state], expectedResult)
-
+	def test_NotAtGoal(self, state, expectedResult):
+		self.assertNumericDictAlmostEqual(self.policy[state], expectedResult}
+	
 	def tearDown(self):
 		pass
-	
+				       
 @ddt
 class TestSetUpLandmark(unittest.TestCase):
 	def setUp(self):
-		landmarkLocation = {"hall": (1,1)}
-		landmarkStateSet = {"hall":[(0, 0), (0, 1), (0, 2), (1, 1), (2, 0), (2, 1), (2, 2), (3, 0), (3, 1), (3, 2)]}
+		gamma = 0.9
+		convergenceTolerance = 0.00001
 
+		stateSet = [(i,j) for i in range(2) for j in range(2)]
+		landmarkLocation = {"h1": (0,0)}
+		landmarkStateSet = {"h1": stateSet}
 		actionSet = [(0,1), (0,-1), (1,0), (-1,0)]
+
 		getTransitionTable = targetCode.tt.CreateTransitionTable(actionSet)
 
 		actionCost = -1
 		goalReward = 10
 		getRewardTable = targetCode.rt.CreateRewardTable(actionSet, actionCost, goalReward)
 
-		gamma = 0.9
-		convergenceTolerance = 0.00001
 		getLandmarkPolicy = targetCode.GetLandmarkPolicy(gamma, convergenceTolerance)
-
 		merge = targetCode.merge
 
+		setup = SetUpLandmark(landmarkLocation, landmarkStateSet, actionSet, getTransitionTable, getRewardTable, getLandmarkPolicy, merge)
 		existingOptions = {}
-		setUp = targetCode.SetUpLandmark(landmarkLocation, landmarkStateSet, actionSet, getTransitionTable, getRewardTable, getLandmarkPolicy, merge)
-		self.policies = setUp(existingOptions)
-	
+		self.result = setup(existingOptions)			  
+						  
 	def assertNumericDictAlmostEqual(self, calculatedDictionary, expectedDictionary, places=7):
 		self.assertEqual(calculatedDictionary.keys(), expectedDictionary.keys())
 		for key in calculatedDictionary.keys():
 		    self.assertAlmostEqual(calculatedDictionary[key], expectedDictionary[key], places=places)
 	
-	@data(((1,1), {(0,1): 0.5, (0,-1): 0.5}))
+	@data(((0, 0), {(0, -1): 0.5, (-1, 0): 0.5}))
 	@unpack
 	def test_AtLandmark(self, state, expectedResult):
 		self.assertNumericDictAlmostEqual(self.policies[state], expectedResult)
 		
-	@data(((3,0), {(-1,0): 0.5, (0,1): 0.5}), ((3,2), {(-1,0): 0.5, (0,-1): 0.5}))
+	@data(((0, 1), {(0, -1): 1.0}),((1, 1), {(0, -1): 0.5, (-1, 0): 0.5}))
 	@unpack
-	def test_Boundary(self, state, expectedResult):
+	def test_NotAtLandmark(self, state, expectedResult):
 		self.assertNumericDictAlmostEqual(self.policies[state], expectedResult)
-		
-	@data(((0,1), {(1,0): 1.0}), ((2,1), {(-1,0): 1.0}))
-	@unpack
-	def test_Inner(self, state, expectedResult):
-		self.assertNumericDictAlmostEqual(self.policies[state], expectedResult)
-		
+	
 	def tearDown(self):
 		pass
  
