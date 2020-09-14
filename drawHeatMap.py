af34a8ed85799a395ea739ce60d276b477da723e
adelchan07 <66882888+adelchan07@users.noreply.github.com>
2020-09-14 16:13:44 -0700

    Update drawHeatMap.py

--- drawHeatMap.py
+++ drawHeatMap.py
@@ -15,19 +15,30 @@ from matplotlib import colors
 import matplotlib
 
 def drawHeatMap(width, height, V, vmin, vmax):
-	
-    data = np.ones((width, height)) * np.nan #fill with placeholders
-
-    for state in V.keys(): #update with V values
+    data = np.ones((width, height)) * np.nan
+    # fill in some fake data
+    for state in V.keys():
         data[state] = V[state]
-	
+    # make a figure + axes
     fig, ax = plt.subplots(1, 1, tight_layout=True)
-	
+    # make color map
     my_cmap = sb.heatmap(data, cmap='RdYlGn', linewidths=0.1, vmin=vmin, vmax=vmax)
-	
     for x in range(N + 1):
         ax.axhline(x, lw=0.3, color='k', zorder=5)
         ax.axvline(x, lw=0.3, color='k', zorder=5) 
+
+def drawArrows(V, mainPolicy, optionPolicies):
+	"""
+		a. primitive option: plt arrow with the single action
+		b. landmark option: follow landmark policy and plot all arrows until landmark is reached
+			--> give each landmark a distinct color and plot arrow with that color
+
+		**need additional function brnaching between drawing a single arrow and drawing landmark arrows (which builds off drawing the single arrow)
+	
+	optionPolicies = {"up": (0,1)... "h1": {policy}}
+	optionColors = {"all primitive options": 'k','h1': 'r', 'h2': 'g'...}
+
+	"""
 
 """
 def drawFinalMap(V, policy, width, height):
