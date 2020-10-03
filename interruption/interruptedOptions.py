746710983c7e552f035ab28a1023cf0bb8236039
adelchan07 <66882888+adelchan07@users.noreply.github.com>
2020-10-02 18:05:05 -0700

    Update interruptedOptions.py

--- interruption/interruptedOptions.py
+++ interruption/interruptedOptions.py
@@ -8,6 +8,11 @@ early termination of options + choosing which option to pick up
 **addition of a landmark option that takes you directly to the ultimate goal state
 
 two functions used to initiate the need for options to be interrupted
+
+purpose of funcitions:
+1. checkCondition: 
+	a. True = needs to check for a possible new option
+	b. False = no need to check, can stay with the current option you are following
 """
 
 class checkCondition(object):
@@ -15,7 +20,7 @@ class checkCondition(object):
 		self.optionSpace = optionSpace
 
 	def __call__(self, state, sPrime):
-		return !(self.optionSpace[state] == self.optionSpace[sPrime])
+		return !(self.optionSpace[state] == self.optionSpace[sPrime]) 
 
 class compareOptions(object):
 	def __init__(self, policy, landmarkOptions):
