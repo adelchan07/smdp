c0a1d12166ebe00c536873d2f5f39ad0dab66f5d
adelchan07 <66882888+adelchan07@users.noreply.github.com>
2020-10-02 18:03:20 -0700

    Add files via upload

---
+++ interruptedOptions.py
@@ -0,0 +1,38 @@
+"""
+Created on Fri Oct 2 12:04:49 2020
+@author: adelphachan
+
+interruptedOptions.py
+
+early termination of options + choosing which option to pick up
+**addition of a landmark option that takes you directly to the ultimate goal state
+
+two functions used to initiate the need for options to be interrupted
+"""
+
+class checkCondition(object):
+	def __init__(self, optionSpace):
+		self.optionSpace = optionSpace
+
+	def __call__(self, state, sPrime):
+		return !(self.optionSpace[state] == self.optionSpace[sPrime])
+
+class compareOptions(object):
+	def __init__(self, policy, landmarkOptions):
+		self.policy = policy
+		self.landmarkOptions = landmarkOptions
+
+	def __call__(self, state, currentOption):
+		options = list(self.policy[state].keys())
+
+		if currentOption in options: #preferable to stick to where you currently are
+			return option
+
+		else:
+			for option in options:
+				if option in landmarkOptions:
+					return option
+
+			
+
+
