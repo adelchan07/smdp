45a7ca465c733702954f95af6acc84ffefa49b37
adelchan07 <66882888+adelchan07@users.noreply.github.com>
2020-10-05 17:31:04 -0700

    Update interruptionTransition.py

--- interruption/interruptionTransition.py
+++ interruption/interruptionTransition.py
@@ -1,1 +1,14 @@
+"""
+Created on Mon Oct 5 10:23:50 2020
+@author: adelphachan
 
+interruptionTransition.py
+
+alter transition function to explore new options whenever new options are available
+pulling from the two functions found in interruptedOptions.py
+"""
+
+class interruptedTransition(object):
+  def __init__(self, optionSPrime, 
+  
+  def __call__(state, option, sPrime):
