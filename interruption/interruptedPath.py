56adc5bd3a90e4bebdf3778ee9e5bfa9ca650ef3
adelchan07 <66882888+adelchan07@users.noreply.github.com>
2020-10-24 17:11:19 -0700

    Update interruptedPath.py

--- interruption/interruptedPath.py
+++ interruption/interruptedPath.py
@@ -1,7 +1,90 @@
 """
-Created on Sun Oct 11 13:03:57 2020
+Created on Tues Oct 20 20:13:34 2020
 @author: adelphachan
 interruptedPath.py
 
 given a state, return the new "policy" including changes caused by interruption
 """
+import numpy as np 
+from interruptOption import CheckCondition
+from interruptOption import CompareOptions
+
+class getOption(object):
+	def __init__(self, optionType, policy):
+		self.optionType = optionType
+		self.policy = policy
+
+	def __call__(self, state):
+		options = self.policy[state]
+
+		for option in options:
+			if self.optionType[option] == 'landmark':
+				return option
+
+class singleStep(object):
+	def __init__(self, optionPolicies, getNextState, stateSet):
+		self.optionPolicies = optionPolicies
+		self.getNextState #getNextState from regular transition function
+		self.stateSet = stateSet
+
+	def __call__(self, state, option):
+		action = list(self.optionPolicies[option][state].keys())[0]
+		return self.getNextState(state, action, self.stateSet)
+
+
+class GetInterruptedPath(object):
+	def __init__(self, optionPolicies, goalState, getOption, singleStep, CheckCondition, CompareOptions):
+		
+		self.optionPolicies = optionPolicies
+		self.goalState = goalState
+
+		self.getOption = getOption
+		self.singleStep = singleStep
+
+		self.checkCondition = CheckCondition
+		self.compareOptions = CompareOptions
+
+	def __call__(self, state):
+		path = {}
+		currentState = state
+		currentOption = self.getOption(state)
+
+		while currentState != self.goalState:
+			specificStep = list(self.optionPolicies[currentOption][state].keys())[0]
+			path[currentState] = specificStep
+
+			sPrime = self.singleStep(currentState, currentOption)
+
+			if self.checkCondition(currentState, sPrime):
+				currentOption = self.compareOptions(sPrime, currentOption)
+			currentState = sPrime
+
+		return path
+
+class GetOptionHistory(object):
+	def __init__(self, optionPolicies, goalState, getOption, singleStep, CheckCondition, CompareOptions):
+		
+		self.optionPolicies = optionPolicies
+		self.goalState = goalState
+
+		self.getOption = getOption
+		self.singleStep = singleStep
+
+		self.checkCondition = CheckCondition
+		self.compareOptions = CompareOptions
+
+	def __call__(self, state):
+		optionHistory = {}
+		currentState = state
+		currentOption = self.getOption(state)
+
+		while currentState != self.goalState:
+			path[currentOption] = currentOption
+
+			sPrime = self.singleStep(currentState, currentOption)
+
+			if self.checkCondition(currentState, sPrime):
+				currentOption = self.compareOptions(sPrime, currentOption)
+			currentState = sPrime
+
+		return optionHistory
