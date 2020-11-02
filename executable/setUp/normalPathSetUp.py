56f2043b5f4608ab1927cce9b011d1e45ec0107f
adelchan07 <66882888+adelchan07@users.noreply.github.com>
2020-11-02 15:01:09 -0800

    Create normalPathSetUp.py
    reorganization: helper classes specific to a grid environment that are needed to initiate GetNormalPath class found in src

---
+++ executable/setUp/normalPathSetUp.py
@@ -0,0 +1,77 @@
+class primitivePolicyPath(object):
+	def __init__(self, primitiveOptions):
+		self.primitiveOptions = primitiveOptions
+
+	def __call__(self, state, option, path):
+		path[state] = self.primitiveOptions[option]
+
+		return path
+
+class landmarkPolicyPath(object):
+	def __init__(self, landmarkPolicies, landmarkTermination):
+		self.landmarkPolicies = landmarkPolicies
+		self.landmarkTermination = landmarkTermination
+
+	def __call__(self, state, option, path):
+		currentState = state 
+		endGoal = self.landmarkTermination[option]
+		policy = self.landmarkPolicies[option]
+
+		while currentState != endGoal:
+			action = list(policy[state].keys())[0] #all optimal --> just pick first one
+			path[currentState] = action
+
+			currentState = (currentState[0] + action[0], currentState[1] + action[1])
+
+		return path
+
+class policyToPath(object):
+	def __init__(self, optionType, primitivePolicyPath, landmarkPolicyPath):
+		self.optionType = optionType
+		self.primitivePolicyPath = primitivePolicyPath
+		self.landmarkPolicyPath = landmarkPolicyPath
+		self.result = {option: self.getFunction(option) for option in self.optionType.keys()}
+    
+    
+	def getFunction(self, option):
+		if self.optionType[option] == 'landmark':
+			return self.landmarkPolicyPath
+		return self.primitivePolicyPath
+    
+	def __call__(self, state, option, path):
+		function = self.result[option]
+		return function(state, option, path)
+
+class primitiveSPrime(object):
+	def __init__(self, stateSet, primitiveOptions):
+		self.stateSet = stateSet
+		self.primitiveOptions = primitiveOptions 
+
+	def __call__(self, state, option):
+		action = self.primitiveOptions[option]
+		newState = (state[0] + action[0], state[1] + action[1])
+		if newState in self.stateSet:
+			return newState
+		return state
+
+class landmarkSPrime(object):
+	def __init__(self, optionTermination):
+		self.optionTermination = optionTermination
+
+	def __call__(self, state, option):
+		return self.optionTermination[option]
+
+class getSPrime(object):
+	def __init__(self, optionType, landmarkSPrime, primitiveSPrime):
+		self.landmarkSPrime = landmarkSPrime
+		self.primitiveSPrime = primitiveSPrime
+		self.optionType = optionType
+		self.optionFunction = {option: self.getFunction(option) for option in optionType.keys()}
+
+	def getFunction(self, option):
+		if self.optionType[option] == 'landmark':
+			return self.landmarkSPrime
+		return self.primitiveSPrime
+	def __call__(self, state, option):
+		function = self.optionFunction[option]
+		return function(state, option)
