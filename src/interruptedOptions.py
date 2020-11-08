523571b9ee4e9dcdd8987476c795909609cf3bcb
adelchan07 <66882888+adelchan07@users.noreply.github.com>
2020-11-07 19:14:51 -0800

    Update interruptedOptions.py
    second class redundant --> removed

--- src/interruptedOptions.py
+++ src/interruptedOptions.py
@@ -9,12 +9,3 @@ class CheckCondition(object):
         next = self.optionSpaceFunction(sPrime)
 
         return (current != next) #return True --> prompt next step if the options available at both states are different
-
-class CompareOptions(object):
-    def __init__(self, optionSpaceFunction):
-        self.optionSpaceFunction = optionSpaceFunction
-
-    def __call__(self, state):
-        availableOptions = self.optionSpace[state]
-
-        return availableOptions[0] #since they are all optimal, can choose any
