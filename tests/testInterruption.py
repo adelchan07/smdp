1bd638a77e9e0ca16795cedf9ad2fc982f2bb1f2
adelchan07 <66882888+adelchan07@users.noreply.github.com>
2020-11-07 19:28:46 -0800

    Update testInterruption.py
    removed test cases for compareOptions class that was also deleted out of redundancy

--- tests/testInterruption.py
+++ tests/testInterruption.py
@@ -41,35 +41,5 @@ class testCheckCondition(unittest.TestCase):
     def tearDown(self):
         pass
 
-@ddt 
-class testCompareOptions(unittest.TestCase):
-    def setUp(self):
-
-        #remove "getValidOptions" function from github repository
-        class optionSpaceFuction(object):
-            def__init__(self, optionSpace):
-                self.optionSpace = optionSpace
-
-            def __call__(self, state):
-                optionSet = self.optionSpace[state]
-                return optionSet
-        
-        optionSpace = {"A": [(i,j) for i in range(3) for j in range(3)], "B": [(i,j) for i in range(1) for j in range(3)]}
-        optionSpaceFunction = optionSpaceFunction(optionSpace)
-        self.CompareOptions = targetCode.CompareOptions(optionSpaceFuncion)
-
-    @data(((2,0), "A", "A"), ((2,2), "A", "A"))
-    @unpack
-    def test_NoChange(self, state, currentOption, expectedResult):
-        self.assertEqual(self.CompareOptions(state, currentOption), expectedResult)
-
-    @data(((1,2), "B", "A"), ((0,0), "A", "B"))
-    @unpack
-    def test_NewOptions(self, state, currentOption, expectedResult):
-        self.assertEqual(self.compareOptions(state, currentOption), expectedResult)
-        
-    def tearDown(self):
-        pass 
-
 if __name__ == '__main__':
 	unittest.main(verbosity=2)
