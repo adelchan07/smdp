051300dcb0954a5446fd4225841d25352607f524
adelchan07 <66882888+adelchan07@users.noreply.github.com>
2020-11-07 19:03:07 -0800

    Update normalPath.py
    update GetNormalPath class to reuse available functions

--- exec/interrupted/normalPath.py
+++ exec/interrupted/normalPath.py
@@ -1,17 +1,24 @@
 class GetNormalPath(object):
-	def __init__(self, policy, goalState, policyToPath, getSPrime):
-		self.policy = policy
+	def __init__(self, landmarkPolicies, interruptionPolicy, optionTerminations, getNextState, goalState, stateSet):
+		self.landmarkPolicies = landmarkPolicies
+		self.interruptionPolicy = interruptionPolicy
+		self.optionTerminations = optionTerminations
+		self.getNextState = getNextState
 		self.goalState = goalState
-		self.policyToPath = policyToPath
-		self.getSPrime = getSPrime
+		self.stateSet = stateSet
 
 	def __call__(self, state):
-		path = {}
 		currentState = state
-        
-		while currentState != self.goalState:
-			option = list(self.policy[currentState].keys())[0] #since all are "optimal", pick randomly
-			path = self.policyToPath(currentState, option, path)
-			currentState = self.getSPrime(currentState, option)
+		path = {}
 
-		return path 
+		while currentState != self.goalState:
+
+			currentOption = list(self.interruptionPolicy[state].keys())[0]
+			termination = self.optionTerminations[currentOption]
+
+			while currentState != termination: #loop within each option ensures option -> completion
+				action = list(self.landmarkPolicies[currentOption][state].keys())[0]
+				path[currentState] = action
+				currentState = self.getNextState(currentState, action, self.stateSet)
+
+		return path
