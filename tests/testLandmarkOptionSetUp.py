a213420bf20948044567b5965c95b875954d9005
adelchan07 <66882888+adelchan07@users.noreply.github.com>
2020-11-11 12:46:36 -0800

    Update testLandmarkOptionSetUp.py
    change path to file

--- tests/testLandmarkOptionSetUp.py
+++ tests/testLandmarkOptionSetUp.py
@@ -13,7 +13,7 @@ merge function tested in testPrimitiveOptions.py
 """
 
 import sys
-sys.path.append('../src/optionSetUp/')
+sys.path.append('../exec/original/optionSetUp/')
 
 import numpy as np
 import unittest
