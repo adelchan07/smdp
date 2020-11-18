c12bc86c8cb7c21558657d1008644aa2e7a25c2a
adelchan07 <66882888+adelchan07@users.noreply.github.com>
2020-11-18 00:55:04 -0800

    Update testInterruptedPath.py

--- tests/testInterruptedPath.py
+++ tests/testInterruptedPath.py
@@ -10,23 +10,25 @@ import interruptedPath as targetCode
 class TestInterruptedPath(unittest.TestCase):
 
 	def setUp(self):
-	    
-	    checkCondition = targetCode.io.checkCondition
-	    landmarkPolicies = {'l': {(0,0): (1,0), (1,0): (0,1), (0,1): (1,0), (1,1): (0,1)}, 'b': {(0,0): (1,0), (1,0): (1,0), (0,1): (1,0), (1,1): (-1,0)}}
-	    interruptionPolicy = {(0,0): 'b', (1,0): 'b', (0,1): 'l', (1,1): 'l'}
-	    optionTerminations = {'l': (1,1)}
-	    getNextState = targetCode.tf.getNextState
-	    goalStates = [(1,1)]
-	    stateSet = [(i,j) for i in range(2) for j in range(2)]
 
-	    self.setUp = targetCode.GetInterruptedPath(checkCondition, landmarkPolicies, interruptionPolicy, optionTerminations, getNextState, goalStates, stateSet)
+		optionSpace = {(0,0): ['b'], (1,0): ["b"], (0,1):['l'], (1,1): ['l']}
+		optionSpaceFunction = lambda x: optionSpace[x]
+		checkCondition = targetCode.io.CheckCondition(optionSpaceFunction)
+		landmarkPolicies = {'l':{(0,0): {(1,0):1.0}, (1,0): {(0,1):1.0}, (0,1): {(1,0):1.0}, (1,1): {(0,1):1.0}}, 'b': {(0,0): {(1,0):1.0}, (1,0): {(1,0):1.0}, (0,1): {(1,0):1.0}, (1,1): {(-1,0):1.0}}}
+		interruptionPolicy = {(0,0): {'b':1.0}, (1,0): {'b':1.0}, (0,1): {'l':1.0}, (1,1): {'l':1.0}}
+		optionTerminations = {'l': (1,1), 'b': (1,0)}
+		getNextState = targetCode.tf.getNextState
+		goalStates = [(1,1)]
+		stateSet = [(i,j) for i in range(2) for j in range(2)]
+
+		self.setUp = targetCode.GetInterruptedPath(checkCondition, landmarkPolicies, interruptionPolicy, optionTerminations, getNextState, goalStates, stateSet)
 	
 	def assertNumericDictAlmostEqual(self, calculatedDictionary, expectedDictionary, places=7):
-    		self.assertEqual(calculatedDictionary.keys(), expectedDictionary.keys())
-    		for key in calculatedDictionary.keys():
-       	 		self.assertAlmostEqual(calculatedDictionary[key], expectedDictionary[key], places=places)
+		self.assertEqual(calculatedDictionary.keys(), expectedDictionary.keys())
+		for key in calculatedDictionary.keys():
+			self.assertAlmostEqual(calculatedDictionary[key], expectedDictionary[key], places=places)
 	
-	@data(((1,1), 'l', (1,1), {}), ((0,0), {(0,0): (1,0), (1,0): (0,1)}))
+	@data(((1,1), 'l', (1,1), {}), ((0,0), 'b', (1,0), {(0,0): (1,0)}))
 	@unpack
 	def test_GetPath00(self, state, currentOption, termination, expectedPath):
    		currentState, path = self.setUp.getPath(state, currentOption, termination, {})
