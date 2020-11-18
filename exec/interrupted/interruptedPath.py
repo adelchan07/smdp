b39aaa3ca2f90eae84946ce0b5414caa16de75f3
adelchan07 <66882888+adelchan07@users.noreply.github.com>
2020-11-18 01:05:03 -0800

    Update interruptedPath.py

--- exec/interrupted/interruptedPath.py
+++ exec/interrupted/interruptedPath.py
@@ -23,13 +23,13 @@ class GetInterruptedPath(object):
 			currentOption = list(self.interruptedPolicy[currentState].keys())[0]
 			termination = self.optionTerminations[currentOption]
 			currentState, path = self.getPath(currentState, currentOption, termination, path)
-		
 		return path
 
 	
 	def getPath(self, state, option, termination, path):
 		currentState = state
 		changeOption = False
+		currentOption = option
 		
 		while changeOption == False and currentState != termination:
 			action = list(self.landmarkPolicies[currentOption][currentState].keys())[0]
@@ -63,6 +63,7 @@ class GetOptionHistory(object):
 	
 	def getRecord(self, state, option, termination, record):
 		currentState = state
+		currentOption = option
 		changeOption = False
 
 		while changeOption == False and currentState != termination:
@@ -74,3 +75,4 @@ class GetOptionHistory(object):
 			changeOption = self.checkCondition(currentState, newState)
 			currentState = newState
 		return (currentState, record)
+    
